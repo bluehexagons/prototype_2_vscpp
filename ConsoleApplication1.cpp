4060ebbb98fae38e914be6abadb66ccf6f583ed0
bluehexagons <bluehexagons@users.noreply.github.com>
2023-12-12 21:47:06 -0600

    Make a simple prime factorization program

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,54 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <format>
+using std::endl, std::string;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using Number = long long;
+
+template <typename T>
+bool from_string(const std::string& str, T& value) {
+    auto [_p, ec] = std::from_chars(str.data(), str.data() + str.size(), value);
+
+    return ec == std::errc();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+template <typename T>
+bool prompt_string(T& value) {
+    string userInput;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    std::getline(std::cin, userInput);
+
+    return from_string(userInput, value);
+}
+
+void findFactors(Number n) {
+    // Use trial division to find factors
+    Number upper = n + 1;
+
+    for (Number i = 1; i < upper; i++) {
+        if (n % i == 0) {
+            std::cout << std::format("{} {} ", i, n / i);
+            upper = n / i;
+        }
+    }
+}
+
+int main() {
+    while (true) {
+        std::cout << "Enter a number to check for factors:" << endl;
+        Number val = 0;
+
+        prompt_string(val);
+
+        if (val == 0) {
+            std::cout << "Exiting";
+            return 0;
+        }
+
+        std::cout << std::format("Let's find factors! {}\n", val);
+
+        findFactors(val);
+        std::cout << endl;
+    }
+}
+
